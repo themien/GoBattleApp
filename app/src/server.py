6bf58d58afa21522ef3b3e317b2e22f67f3e6643
Damiano Cangini <damiano.cangini@gmail.com>
2021-04-07 13:00:56 +0100

    Base app setup

--- app/src/server.py
+++ app/src/server.py
@@ -3,7 +3,8 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello World!"
+    return "Damiano"
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0')
+    # remove debug=True when deployed
+   server.run(debug=True, host='0.0.0.0')
